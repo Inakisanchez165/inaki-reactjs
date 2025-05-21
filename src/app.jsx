fc2af8ea23e7b12fbe1c443a8008cc1ba76b10b4
iñaki <inakisanchez02@outlook.com.ar>
2025-05-21 01:57:05 -0300

    Subida inicial del proyecto con Vite y React

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola, Iñaki!</h1>
+      <p>Tu proyecto con Vite y React está funcionando 🎉</p>
+    </div>
+  );
+}
+
+export default App;
